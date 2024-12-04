733899ffbf8c3f432f48af442602894107494180
Khushbu Khushbu <khushbu0@sheridancollege.ca>
2024-12-03 20:39:01 -0500

    Updated logic

--- src/deliverable/Deliverable.java
+++ src/deliverable/Deliverable.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  */
 public class Deliverable {
 
-     /**  
+    /**  
      * @param args the command line arguments  
      */  
     public static void main(String[] args) {  
