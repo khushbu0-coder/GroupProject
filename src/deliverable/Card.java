5c456e059ff2f89363ea9863116c02f001790ae4
Khushbu Khushbu <khushbu0@sheridancollege.ca>
2024-12-03 20:15:44 -0500

    Used Open/closed principle

--- src/deliverable/Card.java
+++ src/deliverable/Card.java
@@ -2,22 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package deliverable;
+package deliverable;  
 
-/**
- *
- * @author khush
- */
-public abstract class Card {
-    //default modifier for child classes
-
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, 
-     * a regular playing card etc.
-     */
-    @Override
-    public abstract String toString();
-
+/**  
+ * Abstract class representing a generic Card.  
+ * This class follows the **Open/Closed Principle** as it can be extended to create different types of cards.  
+ */  
+public abstract class Card {  
+    /**  
+     * Students should implement this method for their specific child classes.  
+     *  
+     * @return a String representation of a card. Could be an UNO card,   
+     * a regular playing card, etc.  
+     */  
+    @Override  
+    public abstract String toString();  
 }
