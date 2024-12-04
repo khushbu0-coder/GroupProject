880211ca9ba08b582a85bf7d875a3e24acdef01b
Khushbu Khushbu <khushbu0@sheridancollege.ca>
2024-12-03 20:23:19 -0500

    Used Single Responsibility Principle

--- src/deliverable/Game.java
+++ src/deliverable/Game.java
@@ -11,11 +11,10 @@ package deliverable;
 import java.util.ArrayList;  
 
 /**  
- * The class that models your game. You should create a more specific child of this class and instantiate the methods  
- * given.  
+ * Abstract class representing a generic Game.  
+ * This class follows the **Single Responsibility Principle** as it focuses solely on game-related logic.  
  */  
 public abstract class Game {  
-
     private final String name; // the title of the game  
     private ArrayList<Player> players; // the players of the game  
 
@@ -24,36 +23,18 @@ public abstract class Game {
         players = new ArrayList<>(); // Initialize with type safety  
     }  
 
-    /**  
-     * @return the name  
-     */  
     public String getName() {  
         return name;  
     }  
 
-    /**  
-     * @return the players of this game  
-     */  
     public ArrayList<Player> getPlayers() {  
         return players;  
     }  
 
-    /**  
-     * @param players the players of this game  
-     */  
     public void setPlayers(ArrayList<Player> players) {  
         this.players = players;  
     }  
 
-    /**  
-     * Play the game. This might be one method or many method calls depending on your game.  
-     */  
     public abstract void play();  
-
-    /**  
-     * When the game is over, use this method to declare and display a winning player.  
-     */  
     public abstract void declareWinner();  
-
-} // end class  
-
+}  
